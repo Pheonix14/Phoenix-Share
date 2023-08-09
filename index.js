d281f415ea8839795d7378aad2ea6e93e648909d
Pheonix14 <97115569+Pheonix14@users.noreply.github.com>
2023-08-09 17:48:21 +0000

    Update 1.1.30

--- index.js
+++ index.js
@@ -165,13 +165,34 @@ app.post('/upload', authenticate, async (req, res) => {
 
   // Display the file name, download link, and QR code on the upload success page
   res.send(`
-    <h2>File uploaded successfully!</h2>
+  <div class="Download">
+    <h2> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-check-fill" viewBox="0 0 16 16">
+  <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 4.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
+</svg> File uploaded successfully!</h2>
     <p>File name: ${file.name}</p>
     <p>Uploaded By: ${username}</p>
     <p>Download link: <a href="${downloadLink}">${downloadLink}</a></p>
     <img src="${qrCodeImage}" alt="QR Code">
 <p>Download the file before it gets deleted. It will be deleted in 60 minutes.</p>
-    <script></script>
+</div>
+    <style>
+h2 {
+      font-size: 20px;
+      text-align: center;
+      margin-bottom: 20px;
+      color: #333;
+    }
+p {
+  font-size: 10px;
+      margin-bottom: 10px;
+      text-align: center;
+      color: #333;
+}
+img {
+  padding: 5px;
+  width: 120px;
+}
+    </style>
 `);
   logger.info(`${fileName} Is Just Uploaded By ${username}. And It's Have 60min To Get Downloaded.`)
   setTimeout(() => {
